d7420ab7ab7cb20d856877c0866bdf7a4fbbd2db
Firepup Sixfifty <firepyp650@gmail.com>
2023-09-29 23:35:51 +0000

    Actual init commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from os import environ, system
+from build import build
+
+system("clear")
+# joe = system("cat /tmp/updated.txt")
+# if joe != 0:
+#     system("./fix.sh")
+# system("clear")
+print("Should be nothing from here")
+import package.src.firepup650 as fp
+
+print("Until here (Half-second sleep)")
+fp.sleep(.5)
+bob = fp.menu({"Yes":"Y", "No": ""}, "(If owner) Build?")
+system("clear")
+if environ["REPL_OWNER"] == "Firepup650" and bob == "Y":
+    build()
+    exit()
+else:
+    print(fp.menu({"a": "hi", "Some fairly long second test case": ["a", "b", "c"], "A third test": 0}, "Test menu"))
+    fp.console.warn("Test Warning")
+    fp.e("No demo yet!")
