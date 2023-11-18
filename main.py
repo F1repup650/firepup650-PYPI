5e4519301c0131ae0fe8dcdc8fab40fff656fdaf
Firepup Sixfifty <firepyp650@gmail.com>
2023-11-18 10:01:40 -0600

    Formatting

--- main.py
+++ main.py
@@ -10,13 +10,22 @@ print("Should be nothing from here")
 import package.src.firepup650 as fp
 
 print("Until here (Half-second sleep)")
-fp.sleep(.5)
-bob = fp.menu({"Yes":"Y", "No": ""}, "(If owner) Build?")
+fp.sleep(0.5)
+bob = fp.menu({"Yes": "Y", "No": ""}, "(If owner) Build?")
 system("clear")
 if environ["REPL_OWNER"] == "Firepup650" and bob == "Y":
     build()
     exit()
 else:
-    print(fp.menu({"a": "hi", "Some fairly long second test case": ["a", "b", "c"], "A third test": 0}, "Test menu"))
+    print(
+        fp.menu(
+            {
+                "a": "hi",
+                "Some fairly long second test case": ["a", "b", "c"],
+                "A third test": 0,
+            },
+            "Test menu",
+        )
+    )
     fp.console.warn("Test Warning")
     fp.e("No demo yet!")
